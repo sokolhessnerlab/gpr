7b280953b466af9e173bce4f31747c1c7bcdbeb9
JustRBlake <justin.blake@du.edu>
2025-06-16 14:33:18 -0600

    Commenting out "startingRound1"

--- 2_tasks/working_files/rcs_task_folder/rdmTask/rcsRDMmodule.py
+++ 2_tasks/working_files/rcs_task_folder/rdmTask/rcsRDMmodule.py
@@ -1214,9 +1214,9 @@ def rcsRDM(subID, cond1, cond2, cond3, cond4, cond1color, cond2color, cond3color
                 win.flip()
                 event.waitKeys(keyList = ['space'], timeStamped=False)
                 
-                startingRound1.draw()
-                win.flip()
-                core.wait(1)
+               # startingRound1.draw()
+               # win.flip()
+               # core.wait(1)
             
             elif r > 0:
                 round2Prep.draw()
