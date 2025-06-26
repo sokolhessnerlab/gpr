12dec4e7fe0203600d6f605e7004f31305183544
JustRBlake <justin.blake@du.edu>
2025-06-26 08:14:07 -0600

    Changing top left earnings text position

--- 2_tasks/working_files/rcs_task_folder/rdmTask/rcsRDMmodule.py
+++ 2_tasks/working_files/rcs_task_folder/rdmTask/rcsRDMmodule.py
@@ -729,7 +729,8 @@ def rcsRDM(subID, cond1, cond2, cond3, cond4, cond1color, cond2color, cond3color
             color = [1,1,1],
             font='Helvetica',
             height = textHeight * 1.2,
-            pos = topL
+            pos = (-.9, .9)
+            #pos = topL
         )
 
         goalTxt = visual.TextStim(
