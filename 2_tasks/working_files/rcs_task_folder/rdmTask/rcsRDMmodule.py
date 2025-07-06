e7d9f42b6cac7845c19c03fa13b660227d2744a7
JustRBlake <justin.blake@du.edu>
2025-07-06 15:03:31 -0600

    Adding core.wait(2) for controlInstGPR so participants have to wait before proceding with the round

--- 2_tasks/working_files/rcs_task_folder/rdmTask/rcsRDMmodule.py
+++ 2_tasks/working_files/rcs_task_folder/rdmTask/rcsRDMmodule.py
@@ -1543,6 +1543,7 @@ def rcsRDM(subID, cond1, cond2, cond3, cond4, cond1color, cond2color, cond3color
             #instructCount.draw()                    
             win.flip()
                     #event.waitKeys(keyList = ['space'], timeStamped = False) # waiting for key press 
+            core.wait(2)
             event.waitKeys(keyList = ['v', 'n'], timeStamped = False)
 
                     #show page 2 + forced viewing
