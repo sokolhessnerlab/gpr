491d1021549848e13db24acea6267f246a75ee84
Peter Sokol-Hessner <hessner@gmail.com>
2025-10-08 12:15:41 -0600

    initial commit
    Co-Authored-By: JustRBlake <163227201+JustRBlake@users.noreply.github.com>

---
+++ 4_analysis/gpr_analysis.R
@@ -0,0 +1,19 @@
+# GPR Analysis Script
+#
+# Script to process the data collected from the GPR (Goal Pressure
+# and Risk-taking) study in 2025-2026.
+#
+
+# STEP 0: CONNECTIONS ----
+# - Connect to the server
+
+# STEP 1: SET YOUR WORKING DIRECTORY! ----
+# On PSH's computers...
+setwd('/Users/sokolhessner/Documents/gitrepos/gpr/');
+# On JB's computers...
+# setwd('/Users/justinblake/Documents/gpr/');
+
+# STEP 2: then run from here on the same ----
+config = config::get();
+
+# STEP 3: 
