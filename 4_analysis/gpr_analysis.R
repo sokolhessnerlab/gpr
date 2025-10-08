dccc15ac8bbe15bde483bb191b95b810657c3385
Peter Sokol-Hessner <hessner@gmail.com>
2025-10-08 12:25:33 -0600

    loading files
    Co-Authored-By: JustRBlake <163227201+JustRBlake@users.noreply.github.com>

--- 4_analysis/gpr_analysis.R
+++ 4_analysis/gpr_analysis.R
@@ -6,6 +6,7 @@
 
 # STEP 0: CONNECTIONS ----
 # - Connect to the server
+rm(list = ls())
 
 # STEP 1: SET YOUR WORKING DIRECTORY! ----
 # On PSH's computers...
@@ -16,4 +17,22 @@ setwd('/Users/sokolhessner/Documents/gitrepos/gpr/');
 # STEP 2: then run from here on the same ----
 config = config::get();
 
-# STEP 3: 
+# STEP 3: Load pre-processed data files
+setwd(config$path$data$processed)
+
+dmfn = dir(pattern = glob2rx('gpr_processed_decisionmaking_data_*.csv'),full.names = T, recursive = T);
+wmfn = dir(pattern = glob2rx('gpr_processed_workingmemory_data_*.csv'),full.names = T, recursive = T);
+subjlevelwidefn = dir(pattern = glob2rx('gpr_processed_subjlevelwide_data_*.csv'),full.names = T, recursive = T);
+subjlevellongfn = dir(pattern = glob2rx('gpr_processed_subjlevellong_data_*.csv'),full.names = T, recursive = T);
+
+data_dm = read.csv(dmfn[length(dmfn)])
+data_wm = read.csv(wmfn[length(wmfn)])
+data_subjlevel_wide = read.csv(subjlevelwidefn[length(subjlevelwidefn)])
+data_subjlevel_long = read.csv(subjlevellongfn[length(subjlevellongfn)])
+
+num_rdm_trials = 50;
+num_rdm_blocks = 4;
+number_of_dm_trials_per_person = num_rdm_trials * num_rdm_blocks; # static = 50, dynamic = 120
+
+subject_IDs = unique(data_dm$subjectnumber)
+number_of_subjects = length(subject_IDs)
