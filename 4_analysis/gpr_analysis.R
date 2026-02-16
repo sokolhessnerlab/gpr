263a8dc44fbbaeaecb57ec34d5d30ace46b1e4b2
JustRBlake <justin.blake@du.edu>
2026-02-15 17:43:01 -0700

    Changing working directory
    Changing the directory so I can look at the summary data

--- 4_analysis/gpr_analysis.R
+++ 4_analysis/gpr_analysis.R
@@ -10,9 +10,9 @@ rm(list = ls())
 
 # STEP 1: SET YOUR WORKING DIRECTORY! ----
 # On PSH's computers...
-setwd('/Users/sokolhessner/Documents/gitrepos/gpr/');
+#setwd('/Users/sokolhessner/Documents/gitrepos/gpr/');
 # On JB's computers...
-# setwd('/Users/justinblake/Documents/GitHub/gpr/');
+setwd('/Users/justinblake/Documents/GitHub/gpr/');
 
 # STEP 2: Load pre-processed data files ----
 config = config::get();
